af0aa26fc3edc81bc0c8152264a193062386a408
a-shilova <anastasiia.a.shilova@gmail.com>
2020-03-19 01:07:14 +0500

    Solution for task-2 in module-2

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,31 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.buffer = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const splitted = chunk.toString();
+    let i = 0;
+
+    while (i < splitted.length) {
+      if (splitted[i] === os.EOL) {
+        if (this.buffer.length > 0) {
+          this.push(this.buffer);
+          this.buffer = '';
+        }
+      } else {
+        this.buffer += splitted[i];
+      }
+      i += 1;
+    }
+    callback();
   }
 
   _flush(callback) {
+    if (this.buffer.length > 0) {
+      callback(null, this.buffer);
+    }
   }
 }
 
