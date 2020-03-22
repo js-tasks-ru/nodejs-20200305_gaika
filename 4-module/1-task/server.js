ed3f1e85eddc559acc12f996b2512688c24a442c
a-shilova <anastasiia.a.shilova@gmail.com>
2020-03-22 22:00:34 +0500

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,23 +1,44 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fileSystem = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
+  if (pathname.indexOf('/') !== -1) {
+    res.statusCode = 400;
+    return res.end();
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'GET':
+  fileSystem.access(filepath, fileSystem.constants.R_OK, (err) => {
+    if (err) {
+      switch (err.code) {
+        case 'ENOENT':
+          res.statusCode = 404;
+          break;
+        default:
+          res.statusCode = 500;
+      }
+      return res.end();
+    }
 
-      break;
+    switch (req.method) {
+      case 'GET':
+        res.statusCode = 200;
+        const readStream = fileSystem.createReadStream(filepath);
+        readStream.pipe(res);
+        break;
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
+  });
 });
 
 module.exports = server;
