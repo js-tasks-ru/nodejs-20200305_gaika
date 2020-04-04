c8695525dfa96ead9cb233d0ae31dcb92525b1aa
a-shilova <anastasiia.a.shilova@gmail.com>
2020-04-04 22:16:48 +0500

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,31 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const users = new Set();
+
 router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise((resolve) => {
+    users.add(resolve);
+
+    ctx.req.on('aborted', () => {
+      users.delete(resolve);
+      resolve();
+    });
+  });
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (!message) {
+    ctx.throw(400, 'message is not required');
+  }
+  users.forEach((user) => user(message));
+  users.clear();
+
+  ctx.status = 200;
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
